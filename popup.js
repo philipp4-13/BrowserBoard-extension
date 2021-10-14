949e20bb6c35c68fea49ac4dbe92330ba6bfd6e4
philipp4-13 <crimsonhotsun@gmail.com>
2021-10-13 18:35:34 -0700

    Delete popup.js

--- popup.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("abler").onclick = 
-function(e)
-{
-	chrome.storage.local.set({isActive: e.currentTarget.checked});
-}
-chrome.storage.local.get(['isActive'], function(result) {
-  document.getElementById("abler").checked = result.isActive;
-});
